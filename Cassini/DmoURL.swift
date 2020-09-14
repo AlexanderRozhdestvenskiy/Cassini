288d55a6ba37901aa7af9e7275a1426ef6cc3491
AlexanderRozhdestvenskiy <alexrozh1988@gmail.com>
2020-09-14 17:22:03 +0300

    Lecture7

---
+++ Cassini/DmoURL.swift
@@ -0,0 +1,14 @@
+//
+//  DemoURL.swift
+//  Cassini
+//
+//  Created by Alexander Rozhdestvenskiy on 14.09.2020.
+//  Copyright © 2020 AlexRozh. All rights reserved.
+//
+
+import Foundation
+
+struct DemoURL {
+    static let stanford = URL(string: "https://www-media.stanford.edu/wp-content/uploads/2017/03/24182714/about_landing-1.jpg")
+}
+
